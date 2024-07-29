a8560ea3566ed42a65e8b8d90a00eb6c8a695043
E-srinibash <ellasrinibash0@gmail.com>
2024-07-29 18:53:47 +0530

    completed the Herosection

---
+++ src/app/chai/lemon/page.tsx
@@ -0,0 +1,6 @@
+export default function LemonPage(){
+    return(
+           <h1 className="text-2xl text-center  text-cyan-200">Lemon Tea</h1>
+        
+    )
+}
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,5 @@
+export default function ChaiPage(){
+    return (
+        <h1 className="text-2xl text-center text-cyan-200">Chai Page</h1>
+    )
+}
